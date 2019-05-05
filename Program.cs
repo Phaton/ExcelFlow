d37d4cf425d4dc14f708655086a09a07fcbac7b2
Javier Ascunce Azpeitia <javi_ascun@hotmail.com>
2019-05-05 22:00:28 +0200

    Last wednesday to output file name.

--- Program.cs
+++ Program.cs
@@ -16,7 +16,7 @@ namespace ExcelTwineroFlow
         {
             string folder = @"C:\Asignacion\" + DateTime.Now.ToString("yyyyMMdd") + " B1TW";
             System.IO.Directory.CreateDirectory(folder);
-            string outputFile = folder + @"\" + DateTime.Now.ToString("yyyyMMdd") + "_B1TW_Cargar.csv"; 
+            string outputFile = folder + @"\" + DateTime.Now.ToString("yyyyMMdd") + "_B1TW_Cargar_"+ UltimoMiercoles()+ ".csv"; 
             ReadExcel(@"C:\Users\Usuario\Documents\Book1.xlsx", outputFile);
             Console.ReadLine();
         }
@@ -26,6 +26,7 @@ namespace ExcelTwineroFlow
                 //I hate Excel
                 Excel.Application _xlApp = new Excel.Application();
                 Excel.Workbook _xlWorkbook = _xlApp.Workbooks.Open(_pathInput);
+                //Sheet index start at 1
                 Excel._Worksheet _xlWorksheet = _xlWorkbook.Sheets[1];
                 Excel.Range _xlRange = _xlWorksheet.UsedRange;
                 int rowCount = _xlRange.Rows.Count;
@@ -33,6 +34,7 @@ namespace ExcelTwineroFlow
                 int[] _puntero = new int[] { 0, 19, 15, 16, 22, 24, 25, 23, 21, 17, 18, colCount, 2, 3, colCount, 26, 5, 8, 9, 10, 11, colCount, colCount, 14, 20, colCount, colCount };
             try
             {
+                //I have to create the FIleStream so I can set the encoding to the system's default, or Excel won't read propertly special characters like "ñ"
                 FileStream fs = new FileStream(_pathOutput, FileMode.Append);
                 using (StreamWriter writer = new StreamWriter(fs, Encoding.Default))
                 {
@@ -114,6 +116,7 @@ namespace ExcelTwineroFlow
             {
                 return _fechain;
             }
+            //Case for the magnificient Excel's date format
             else if (Int32.TryParse(_fechain, out fechadia))
             {
                 return Convert.ToDateTime("1900-01-01").AddDays(fechadia).ToString("dd/MM/yyyy");
@@ -125,6 +128,7 @@ namespace ExcelTwineroFlow
         }
         static string DepuraProv(string _input)
         {
+            //Hardcoded Postal Code-State array.
             string[] _cpProv = new string[] 
             {
                 "Alava",
@@ -252,10 +256,34 @@ namespace ExcelTwineroFlow
         }
         static string RemoveChars (string _input)
         {
+            //I don't want those dirty characters in my db.
             string _output = _input.Replace(";", ",");
             _output = _output.Replace('"', '´');
             _output = _output.Replace("#", " ");
             return _output;
+        }
+        static string UltimoMiercoles()
+        {
+            //We should be getting Wednesday file on Friday, so if we are running this on Wednesday or Thursday, it's last week's file.
+            switch (DateTime.Now.DayOfWeek)
+            {
+                case DayOfWeek.Sunday:
+                    return DateTime.Now.AddDays(-4).ToString("yyyyMMdd");
+                case DayOfWeek.Monday:
+                    return DateTime.Now.AddDays(-5).ToString("yyyyMMdd");
+                case DayOfWeek.Tuesday:
+                    return DateTime.Now.AddDays(-6).ToString("yyyyMMdd");
+                case DayOfWeek.Wednesday:
+                    return DateTime.Now.AddDays(-7).ToString("yyyyMMdd");
+                case DayOfWeek.Thursday:
+                    return DateTime.Now.AddDays(-8).ToString("yyyyMMdd");
+                case DayOfWeek.Friday:
+                    return DateTime.Now.AddDays(-2).ToString("yyyyMMdd");
+                case DayOfWeek.Saturday:
+                    return DateTime.Now.AddDays(-3).ToString("yyyyMMdd");
+                default:
+                    throw new Exception("EVERYTHING BROKE");
+            }
         }
     }
 }
